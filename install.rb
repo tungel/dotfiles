5c51af321cfd762af7c882f7d423b2d3eb566aef
everbot <everbot@live.com>
2015-09-01 21:24:32 +0930

    Install script: remove bash script, use Ruby script
    Also, update `config` file format

--- install.rb
+++ install.rb
@@ -1,3 +1,9 @@
+#!/usr/bin/env ruby
+
+# https://github.com/everbot/dotfiles
+
+require 'io/console' # for STDIN.getch
+
 # help to print colorized string to terminal
 class String
   def colorize(clrCode)
@@ -21,6 +27,76 @@ def fail(msg)
   puts "[ FAILED ] ".clrRed + msg
 end
 
+def success(msg)
+  puts "[ OK ] ".clrGreen + msg
+end
+
+# Create symlink from `source` to `target`
+def linkFile(source, target)
+  skip = false
+  overwrite = false
+  backup = false
+
+  # broken symlink doesn't get detected by `File.exist?`
+  if File.exist?(target) || File.symlink?(target)
+    if File.exist?(target) && File.realpath(target) == source
+      skip = true
+    else
+      if not (@skipAll || @overwriteAll || @backupAll)
+        puts "[ ? ]".clrYellow + " File already exists: #{target}, what do you want to do?"
+        print "      [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all? "
+
+        selection = STDIN.getch
+        puts
+        case selection
+        when "s"
+          skip = true
+        when "S"
+          @skipAll = true
+        when "o"
+          overwrite = true
+        when 'O'
+          @overwriteAll = true
+        when 'b'
+          backup = true
+        when 'B'
+          @backupAll = true
+        else
+          skip = true
+        end
+      end # end checking no "*all" option is selected
+
+      skip |= @skipAll
+      overwrite |= @overwriteAll
+      backup |= @backupAll
+
+      if overwrite
+        # TODO: optimize this
+        File.delete(target)
+        success("removed #{target}")
+      end
+
+      if backup
+        File.rename(target, "#{target}.backup")
+        success("moved #{target} to #{target}.backup")
+      end
+
+      if skip
+        success("skipped #{source}")
+      end
+    end # end checking source == target
+  end
+
+  if !skip
+    begin
+      File.symlink(source, target)
+      success("linked #{source} to #{target}")
+    rescue => err
+      fail("Exception: #{err}")
+    end
+  end
+end
+
 def readConfigFile
   begin
     file = File.new(File.join(SOURCE_DIR, CONFIG_FILE), "r")
@@ -32,8 +108,7 @@ def readConfigFile
     end
     file.close
   rescue => err
-    puts "Exception: #{err}"
-    err
+    fail("Exception: #{err}")
   end
 end
 
@@ -41,33 +116,33 @@ def installDotfiles
   readConfigFile
 
   # loop through all the item with postfix "symlink"
-  Dir.glob(File.join(SOURCE_DIR, "**/*.symlink"), File::FNM_DOTMATCH) do |item|
+  Dir.glob(File.join(SOURCE_DIR, "**/*.symlink"), File::FNM_DOTMATCH) do |source|
     # get the base name without postfix ".symlink"
-    basename = File.basename(item).match(/(?:(?!\.symlink).)*/)[0]
+    basename = File.basename(source).match(/(?:(?!\.symlink).)*/)[0]
 
     if @toSymlink.include?(basename)
-      subDir = File.dirname(item).sub(SOURCE_DIR, '').delete("/")
+      subDir = File.dirname(source).sub(SOURCE_DIR, '').delete("/")
 
       dest = (subDir.length == 0) ? File.join(TARGET, basename) : 
                                     File.join(TARGET, subDir, basename)
 
-      puts basename + " " + dest
+      linkFile(source, dest)
+      # puts basename + " " + dest
     end
   end
 end
 
 CONFIG_FILE = "config"
+TARGET = File.expand_path('~') # user's home dir
+# TARGET = "/home/tung/mytemp/test" # for testing
 
 # get absolute path of the dotfiles repo
 SOURCE_DIR = File.expand_path File.dirname(__FILE__)
 
-TARGET = "/home/mytemp/"
-
 @toSymlink = []
+@skipAll = false
+@overwriteAll = false
+@backupAll = false
 installDotfiles
-
 puts "Number of config files = #{@toSymlink.count}"
-# @toSymlink.each do |str|
-#   puts str
-# end
 
