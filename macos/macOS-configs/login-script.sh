5b5fa6c1b276ab57c7ae9646278caa2e09f8597f
Tung Le <everbot@live.com>
2020-08-02 19:44:55 +1000

    macOS: improve key mapping startup script

--- macos/macOS-configs/login-script.sh
+++ macos/macOS-configs/login-script.sh
@@ -5,13 +5,18 @@
 # - Remapping Keys in macOS 10.12 Sierra
 #   https://developer.apple.com/library/archive/technotes/tn2450/_index.html#//apple_ref/doc/uid/DTS40017618-CH1-KEY_TABLE_USAGES
 
+# map caps lock to left control key for all keyboards
+hidutil property --set '{"UserKeyMapping":[
+{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0}
+]}'
+
 # map caps lock (39) to left control key (E0)
 # swap CMD (E3) and Alt (E2) keys on the left side of the keyboard
 # only perform keys mapping on the Apple's internal keyboard (VendorID: 0x5ac)
 # to view list of keyboards, run: hidutil list -m keyboard
-# hidutil property --matching '{"VendorID":0x5ac}' --set '{"UserKeyMapping":[
-# {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},
-# {"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2},
-# {"HIDKeyboardModifierMappingSrc":0x7000000E2,"HIDKeyboardModifierMappingDst":0x7000000E3}
-# ]}'
+hidutil property --matching '{"VendorID":0x5ac}' --set '{"UserKeyMapping":[
+{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},
+{"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2},
+{"HIDKeyboardModifierMappingSrc":0x7000000E2,"HIDKeyboardModifierMappingDst":0x7000000E3}
+]}'
 
