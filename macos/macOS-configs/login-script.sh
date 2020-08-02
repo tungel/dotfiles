bec25b24e28a5034cc7883431343e914a987122d
Tung Le <everbot@live.com>
2020-08-02 14:18:32 +1000

    macOS: fix key mapping script

--- macos/macOS-configs/login-script.sh
+++ macos/macOS-configs/login-script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# ref: https://stackoverflow.com/questions/127591/using-caps-lock-as-esc-in-mac-os-x/40254864
+# ref:
+# - https://stackoverflow.com/questions/127591/using-caps-lock-as-esc-in-mac-os-x/40254864
+# - Remapping Keys in macOS 10.12 Sierra
+#   https://developer.apple.com/library/archive/technotes/tn2450/_index.html#//apple_ref/doc/uid/DTS40017618-CH1-KEY_TABLE_USAGES
 
-# map caps lock to left control key
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0}]}'
-
-# swap CMD and Alt keys on the left side of the keyboard
-# map left CMD (E3) key to left Alt (E2)
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2}]}'
-# map left Alt (E2) key to left CMD (E3)
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E2,"HIDKeyboardModifierMappingDst":0x7000000E3}]}'
+# map caps lock (39) to left control key (E0)
+# swap CMD (E3) and Alt (E2) keys on the left side of the keyboard
+hidutil property --set '{"UserKeyMapping":[
+{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},
+{"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2},
+{"HIDKeyboardModifierMappingSrc":0x7000000E2,"HIDKeyboardModifierMappingDst":0x7000000E3}
+]}'
 
