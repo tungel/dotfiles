f26ee9410f0aeb81f777906c2fa5f51dbaca7fbc
Tung Le <everbot@live.com>
2020-08-02 13:42:00 +1000

    macOS: swap CMD and Alt keys on the left side of the keyboard

--- macos/macOS-configs/login-script.sh
+++ macos/macOS-configs/login-script.sh
@@ -5,3 +5,9 @@
 # map caps lock to left control key
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0}]}'
 
+# swap CMD and Alt keys on the left side of the keyboard
+# map left CMD (E3) key to left Alt (E2)
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E3,"HIDKeyboardModifierMappingDst":0x7000000E2}]}'
+# map left Alt (E2) key to left CMD (E3)
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E2,"HIDKeyboardModifierMappingDst":0x7000000E3}]}'
+
