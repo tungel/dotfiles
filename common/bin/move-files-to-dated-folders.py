aaeae99b83be50e75ad312404f0ce21cee2eb88b
Tung Le <everbot@live.com>
2020-08-16 20:35:52 +1000

    Update move-files-to-dated-folders.py to process yyyy-mm-dd file names

--- common/bin/move-files-to-dated-folders.py
+++ common/bin/move-files-to-dated-folders.py
@@ -19,27 +19,38 @@ def main(output_dir=None, no_dry_run=None):
 
     # the first 8 chars must be in the form yyyymmdd
     dmy_matcher = re.compile("^\d{8}")
+    # or in the form yyyy-mm-dd
+    dmy_dash_matcher = re.compile("^\d{4}-\d{2}-\d{2}")
+
     cwd = os.getcwd()
 
     for item in os.listdir(cwd):
+        # full path of original file
+        current_file_path = os.path.join(os.getcwd(), item)
+
+        found_match = False
+
         if dmy_matcher.match(item):
-
-            # full path of original file
-            current_file_path = os.path.join(os.getcwd(), item)
-
             target_dir_name = f"{item[0:4]}-{item[4:6]}"  # yyyy-mm
-            target_dir_path = os.path.join(output_dir, target_dir_name)
+            found_match = True
+        elif dmy_dash_matcher.match(item):
+            target_dir_name = f"{item[0:4]}-{item[5:7]}"  # yyyy-mm
+            found_match = True
 
-            try:
-                os.makedirs(target_dir_path)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise
+        if not found_match:
+            continue
 
-            print(f"Moving {current_file_path} to {target_dir_path}")
+        target_dir_path = os.path.join(output_dir, target_dir_name)
+        try:
+            os.makedirs(target_dir_path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
-            if no_dry_run:
-                shutil.move(current_file_path, target_dir_path)
+        print(f"Moving {current_file_path} to {target_dir_path}")
+
+        if no_dry_run:
+            shutil.move(current_file_path, target_dir_path)
 
     if no_dry_run:
         print('\n\nSuccessfully moved files!')
