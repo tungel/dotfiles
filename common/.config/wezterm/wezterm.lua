57da9bb10a3e46637df329dde1570c8485d958b8
Tung Le <tung@macbookpro>
2021-09-19 22:07:48 +1000

    Add wezterm config file and other stuff

---
+++ common/.config/wezterm/wezterm.lua
@@ -0,0 +1,47 @@
+local wezterm = require 'wezterm';
+return {
+  font = wezterm.font("FantasqueSansMono Nerd Font Mono"),
+  font_size=16.0,
+
+  -- color_scheme = "MaterialOcean",
+
+  keys = {
+    {key='1', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;49~'}},
+    {key='2', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;50~'}},
+    {key='3', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;51~'}},
+    {key='4', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;52~'}},
+    {key='5', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;53~'}},
+    {key='6', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;54~'}},
+    {key='7', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;55~'}},
+    {key='8', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;56~'}},
+    {key='9', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;57~'}},
+    {key='0', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;48~'}},
+    {key='Tab', mods='CTRL',            action=wezterm.action{SendString='\x1b[27;5;9~'}},
+  },
+
+
+  -- https://github.com/wez/wezterm/tree/main/assets/colors
+  -- color_scheme = "Tomorrow Night",
+  -- The color scheme below is based on the Tomorrow Night theme. The background
+  -- color is customized to make it a bit brighter.
+  colors = {
+    background = "#242629",
+    -- background = "#18191b",
+    foreground = "#c5c8c6",
+
+    cursor_bg = "#c5c8c6",
+    cursor_border = "#c5c8c6",
+    cursor_fg = "#1d1f21",
+    selection_bg = "#373b41",
+    selection_fg = "#c5c8c6",
+
+    -- ansi black is customized to #1d1f21
+    -- ansi = {"#1d1f21","#cc6666","#b5bd68","#f0c674","#81a2be","#b294bb","#8abeb7","#ffffff"},
+    -- brights = {"#666666","#cc6666","#b5bd68","#f0c674","#81a2be","#b294bb","#8abeb7","#ffffff"},
+    ansi = {"#1d1f21","#cc6666","#b5bd68","#e6c547","#81a2be","#b294bb","#70c0ba","#373b41"},
+    brights = {"#666666","#ff3334","#9ec400","#f0c674","#81a2be","#b77ee0","#54ced6","#282a2e"},
+  },
+
+  term = "wezterm",
+}
+
--- common/bin/fixsophos.sh
+++ common/bin/fixsophos.sh
@@ -20,9 +20,20 @@ while true; do
     echo "We are fine for now"
   fi
 
+  echo "-----------------------------------------------------------------------"
   echo "Killing Sophos Network Extension..."
   sudo kill -9 `ps aux | grep 'Sophos Network Extension' | grep '_sophos' | awk '{ print $2 }'`
   echo "Done killing Sophos Network Extension."
+
+  echo "-----------------------------------------------------------------------"
+  echo "Killing SophosScanD..."
+  sudo kill -9 `ps aux | grep 'SophosScanD' | grep '_sophos' | awk '{ print $2 }'`
+  echo "Done killing SophosScanD."
+
+  echo "-----------------------------------------------------------------------"
+  echo "Killing SophosEventMonitor..."
+  sudo kill -9 `ps aux | grep 'SophosEventMonitor' | grep '_sophos' | awk '{ print $2 }'`
+  echo "Done killing SophosEventMonitor."
 
   sleep 1
 done
