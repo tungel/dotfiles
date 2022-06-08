09c421d4f4ddee215913ad9fa3d247903ba81737
Tung Le <tungel@users.noreply.github.com>
2022-06-08 21:31:24 +1000

    Update lvim config

--- common/.config/lvim/config.lua
+++ common/.config/lvim/config.lua
@@ -53,7 +53,7 @@ lvim.builtin.which_key.mappings["t"] = {
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+-- lvim.builtin.nvimtree.show_icons.git = 0 -- comment out since it's now an invalid config
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
