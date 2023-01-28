598879cb1bd1d65f4d088e0e73cb5a9006e27143
Tung Le <tungel@users.noreply.github.com>
2023-01-28 20:28:33 +1100

    Preserve some metadata in ffmpeg script

--- common/bin/ffmpeg-process.sh
+++ common/bin/ffmpeg-process.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# https://video.stackexchange.com/questions/23741/how-to-prevent-ffmpeg-from-dropping-metadata
+
 mkdir processed
 for i in *.MP4; do
   file_name_without_extension=$(echo `basename $i` | cut -f 1 -d '.')
-  ffmpeg -i $i -vcodec libx265 -crf 24 processed/$file_name_without_extension-processed-ffmpeg.MP4
+  ffmpeg -i $i -movflags use_metadata_tags -map_metadata 0 -vcodec libx265 -crf 24 processed/$file_name_without_extension-processed-ffmpeg.MP4
 done
 
