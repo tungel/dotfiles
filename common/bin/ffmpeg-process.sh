b9d01ef38b9d8077385ecf93b840dd0a540247b7
Tung Le <tungel@users.noreply.github.com>
2023-02-27 04:49:56 +1100

    ffmpeg script: process case insensitive mp4

--- common/bin/ffmpeg-process.sh
+++ common/bin/ffmpeg-process.sh
@@ -3,8 +3,12 @@
 # https://video.stackexchange.com/questions/23741/how-to-prevent-ffmpeg-from-dropping-metadata
 
 mkdir processed
-for i in *.MP4; do
+
+# process file mp4, MP4, ...
+for i in *.[mM][pP]4; do
   file_name_without_extension=$(echo `basename $i` | cut -f 1 -d '.')
-  ffmpeg -i $i -movflags use_metadata_tags -map_metadata 0 -vcodec libx265 -preset slow -crf 22 processed/$file_name_without_extension-ffmpeg.MP4
+  # `-preset slow` is really slow
+  # ffmpeg -i $i -movflags use_metadata_tags -map_metadata 0 -vcodec libx265 -preset slow -crf 22 processed/$file_name_without_extension-ffmpeg.MP4
+  ffmpeg -i $i -movflags use_metadata_tags -map_metadata 0 -vcodec libx265 -crf 22 processed/$file_name_without_extension-ffmpeg.MP4
 done
 
