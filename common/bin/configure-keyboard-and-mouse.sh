96d4bec705ee741b46711d058a24155cbd9f41c3
Tung Le <tungel@users.noreply.github.com>
2023-04-29 17:58:26 +1000

    Fix natural scrolling prop

--- common/bin/configure-keyboard-and-mouse.sh
+++ common/bin/configure-keyboard-and-mouse.sh
@@ -6,7 +6,7 @@ xset r rate 200 30; xmodmap ~/.Xmodmap
 # disable external mouse natural scrolling
 device_id=$(xinput list --id-only 'Microsoft Microsoft® 2.4GHz Transceiver v9.0 Mouse')
 if [[ -n "$device_id" ]]; then
-  xinput set-prop $device_id 318 0
+  xinput set-prop $device_id 'libinput Natural Scrolling Enabled' 0
 else
   echo "No known external mouse is connected."
 fi
