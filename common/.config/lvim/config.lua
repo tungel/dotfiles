33ef4018a84b893536d5267f73ffa47ed6651171
Tung Le <tungel@users.noreply.github.com>
2023-04-29 21:16:35 +1000

    lvim: c-t to toggle terminal window

--- common/.config/lvim/config.lua
+++ common/.config/lvim/config.lua
@@ -233,6 +233,9 @@ vim.cmd([[
 lvim.keys.term_mode["<C-h>"] = false              -- disable C-h mapping so that it erases left char instead of hiding the term
 lvim.keys.term_mode["<C-[><C-[>"] = "<C-\\><C-N>" -- press C-[ two times to switch to move mod in terminal
 
+-- https://www.lunarvim.org/docs/configuration/keybindings#toggleterm-terminal-mappings
+lvim.builtin.terminal.open_mapping = "<c-t>"
+
 lvim.keys.normal_mode["<C-]>"] = ":Telescope lsp_definitions<CR>"
 
 -- use Ctrl + l to redraw the screen and remove any search highlighs
